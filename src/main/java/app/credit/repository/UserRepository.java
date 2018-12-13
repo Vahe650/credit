fe70b41753b6312cb9d71b043f878a2aa1306a54
Vahe <vahesimonyan650@gmail.com>
2018-12-13 09:56:09 +0400

    1

--- src/main/java/app/credit/repository/UserRepository.java
+++ src/main/java/app/credit/repository/UserRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 
-    @Query("SELECT t FROM User t WHERE LOWER(t.name) LIKE LOWER(CONCAT('%', :searchItem, '%')) OR LOWER(t.country)" +
+    @Query( "SELECT t FROM User t WHERE LOWER(t.name) LIKE LOWER(CONCAT('%', :searchItem, '%')) OR LOWER(t.country)" +
             "LIKE LOWER(CONCAT('%', :searchItem, '%')) ORDER BY t.name ASC\n" + "  ")
     List<User> findUserByNameLike(@Param("searchItem") String find);
 
